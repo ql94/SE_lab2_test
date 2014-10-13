f1d529bc021ccbeae6bdaf007d5a88c30fdf2509
ql94 <ql941016@gmail.com>
2014-10-13 08:23:02 +0800

    Create Node.java
    Node

---
+++ Node.java
@@ -0,0 +1,13 @@
+class Node {
+	public String name;
+	public double qos;
+	public double reliability;
+	public double price;
+
+	public Node() {
+		reliability = 0;
+		price = 0;
+		qos = 0;
+	}
+}
+
